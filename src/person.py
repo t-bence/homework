26f343061d2899cc9f7b7c320f810437347ba304
t-bence <toth.bence.mihaly@gmail.com>
2023-09-02 14:18:44 +0200

    Time parsing done

--- src/person.py
+++ src/person.py
@@ -11,14 +11,17 @@ class Person:
         Parse a datetime from a formatted string
         Input string format: 2023-01-31T08:18:36.000Z
         """
-        return datetime.now()
+
+        # this is basically an ISO format
+        # we just need to remove the Z from the end
+        return datetime.fromisoformat(timestamp[:-1])
 
 
 
     def add_event(self, user: str, direction: str, event_time: str) -> None:
         """Store a check-in event"""
         is_check_in = direction.lower() == "gate_in"
-        time = None
+        time = self.parse_time(event_time)
 
         self.events.append((user, is_check_in, time))
         pass
--- test/test_person.py
+++ test/test_person.py
@@ -9,9 +9,8 @@ class Tests(unittest.TestCase):
 
     def test_person_time_parsing(self):
         person = Person("Bela")
-        timestamp = "2023-01-31T08:18:36.000Z"
-
-        time = person.parse_time(timestamp)
+        
+        time = person.parse_time("2023-01-31T08:18:36.000Z")
 
         self.assertEqual(time.year, 2023)
         self.assertEqual(time.month, 1)
@@ -20,6 +19,20 @@ class Tests(unittest.TestCase):
         self.assertEqual(time.minute, 18)
         self.assertEqual(time.second, 36)
         self.assertEqual(time.microsecond, 0)
+    
+
+    def test_person_time_parsing2(self):
+        person = Person("Bela")
+        
+        time = person.parse_time("2023-03-03T20:24:54.500Z")
+        self.assertEqual(time.year, 2023)
+        self.assertEqual(time.month, 3)
+        self.assertEqual(time.day, 3)
+        self.assertEqual(time.hour, 20)
+        self.assertEqual(time.minute, 24)
+        self.assertEqual(time.second, 54)
+        self.assertEqual(time.microsecond, 500_000)
+
 
 if __name__ == '__main__':
     unittest.main()

d8b6205e644374ffbbcd6838da3b9a96d0d0a0d6
t-bence <toth.bence.mihaly@gmail.com>
2023-09-02 14:10:09 +0200

    Datetime parsing test ready

--- src/person.py
+++ src/person.py
@@ -1,6 +1,24 @@
-class Person:
-    def __init__(self) -> None:
-        pass
+from datetime import datetime
 
-    def greet(self) -> str:
-        return "Hello"
+class Person:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.events = []
+
+
+    def parse_time(self, timestamp: str) -> datetime:
+        """
+        Parse a datetime from a formatted string
+        Input string format: 2023-01-31T08:18:36.000Z
+        """
+        return datetime.now()
+
+
+
+    def add_event(self, user: str, direction: str, event_time: str) -> None:
+        """Store a check-in event"""
+        is_check_in = direction.lower() == "gate_in"
+        time = None
+
+        self.events.append((user, is_check_in, time))
+        pass
--- test/test_person.py
+++ test/test_person.py
@@ -7,9 +7,19 @@ from src.person import Person
 
 class Tests(unittest.TestCase):
 
-    def test_person_greeting(self):
-        person = Person()
-        self.assertEqual(person.greet(), "Hello")
+    def test_person_time_parsing(self):
+        person = Person("Bela")
+        timestamp = "2023-01-31T08:18:36.000Z"
+
+        time = person.parse_time(timestamp)
+
+        self.assertEqual(time.year, 2023)
+        self.assertEqual(time.month, 1)
+        self.assertEqual(time.day, 31)
+        self.assertEqual(time.hour, 8)
+        self.assertEqual(time.minute, 18)
+        self.assertEqual(time.second, 36)
+        self.assertEqual(time.microsecond, 0)
 
 if __name__ == '__main__':
     unittest.main()

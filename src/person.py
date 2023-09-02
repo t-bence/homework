99a685dbbccb7bc25bb70819d06d679b13002ea7
t-bence <toth.bence.mihaly@gmail.com>
2023-09-03 00:08:32 +0200

    Compute office stays

--- src/person.py
+++ src/person.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import List, Tuple
 
 
 class Person:
     def __init__(self, name: str) -> None:
         self.name = name
         self.events = []
+        self.office_stays: List[Tuple[datetime, timedelta]] = []
 
     def add_event(self, direction: str, timestamp: str) -> None:
         """Store a check-in or check-out event"""
@@ -16,3 +18,20 @@ class Person:
         event_time = datetime.fromisoformat(timestamp[:-1])
 
         self.events.append((is_check_in, event_time))
+
+    def compute_office_stays(self) -> None:
+        """Compute stays in the office from the events."""
+        last_check_in = None  # datetime of last checkin, if checked in currently, else None
+
+        for is_check_in, event_time in self.events:
+            if is_check_in and last_check_in is None:
+                # checking in
+                last_check_in = event_time
+            elif not is_check_in and last_check_in is not None:
+                # checking out, office stay ends now
+                length = last_check_in - event_time
+                self.office_stays.append((last_check_in, event_time - last_check_in))
+                last_check_in = None
+            else:
+                raise ValueError(f"Inconsistent checking at {event_time}")
+
--- tests/test_person.py
+++ tests/test_person.py
@@ -25,6 +25,29 @@ class PersonTests(unittest.TestCase):
         self.assertEqual(time.second, 36)
         self.assertEqual(time.microsecond, 0)
 
+    def test_person_office_stay_calculation(self):
+        person = Person("Bela")
+
+        person.add_event("GATE_IN", "2023-01-31T08:18:36.000Z")
+        person.add_event("GATE_OUT", "2023-01-31T09:18:36.000Z")
+
+        person.compute_office_stays()
+
+        start, length = person.office_stays[0]
+
+        self.assertEqual(length.total_seconds(), 60*60)
+
+        self.assertEqual(start.year, 2023)
+        self.assertEqual(start.month, 1)
+        self.assertEqual(start.day, 31)
+        self.assertEqual(start.hour, 8)
+        self.assertEqual(start.minute, 18)
+        self.assertEqual(start.second, 36)
+        self.assertEqual(start.microsecond, 0)
+
+    def test_person_stays_in_February(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()

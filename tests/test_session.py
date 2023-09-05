7a8376e64dd65090dc38533c942e0d2a84201fa4
t-bence <toth.bence.mihaly@gmail.com>
2023-09-05 19:47:52 +0200

    Test for session from pieces

--- tests/test_session.py
+++ tests/test_session.py
@@ -13,12 +13,37 @@ class SessionTests(unittest.TestCase):
     def test_session_simplest(self):
         sc = SessionCounter(timedelta(hours=1))
 
-        stay = [OfficeStay(datetime(2023, 2, 15, 20),
+        stays = [OfficeStay(datetime(2023, 2, 15, 20),
                            datetime(2023, 2, 15, 21, 30))]
 
-        lengths = sc.compute_length_in_hours(stay)
+        lengths = sc.compute_length_in_hours(stays)
 
         self.assertEqual(lengths[0], 1.5)
+
+    def test_compute_longest_session(self):
+        # create a counter with 2 hours of max break
+        sc = SessionCounter(timedelta(hours=2))
+
+        stays = [
+            # add a long session from two parts with <2 hours break:
+            # 20-21.30 and 23-1 at next day, total: 5 hours
+            OfficeStay(
+                datetime(2023, 2, 15, 20),
+                datetime(2023, 2, 15, 21, 30)),
+            OfficeStay(
+                datetime(2023, 2, 15, 23),
+                datetime(2023, 2, 16, 1)),
+
+            # and add one session that is shorter than the total,
+            # but longer then each segment separately
+            OfficeStay(
+                datetime(2023, 2, 15, 23),
+                datetime(2023, 2, 16, 2))
+        ]
+
+        lengths = sc.compute_length_in_hours(stays)
+
+        self.assertEqual(lengths, [5, 3])
 
 
 if __name__ == '__main__':
